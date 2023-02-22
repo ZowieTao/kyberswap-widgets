1bd35af1da45baa5a8dc02be9053e45c83b4b242
zowie <zowietao@gmail.com>
2023-02-22 14:40:00 +0800

    use tailwind

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,3 +1,53 @@
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+import '@/styles/globals.css';
+import { WagmiConfig, createClient, configureChains, mainnet } from 'wagmi';
+
+import { AppProps } from 'next/app';
+
+import { alchemyProvider } from 'wagmi/providers/alchemy';
+import { publicProvider } from 'wagmi/providers/public';
+
+import { CoinbaseWalletConnector } from 'wagmi/connectors/coinbaseWallet';
+import { InjectedConnector } from 'wagmi/connectors/injected';
+import { MetaMaskConnector } from 'wagmi/connectors/metaMask';
+import { WalletConnectConnector } from 'wagmi/connectors/walletConnect';
+
+const { chains, provider, webSocketProvider } = configureChains(
+  [mainnet],
+  [alchemyProvider({ apiKey: 'yourAlchemyApiKey' }), publicProvider()],
+);
+
+const client = createClient({
+  autoConnect: true,
+  connectors: [
+    new MetaMaskConnector({ chains }),
+    new CoinbaseWalletConnector({
+      chains,
+      options: {
+        appName: 'opencord',
+      },
+    }),
+    new WalletConnectConnector({
+      chains,
+      options: {
+        qrcode: true,
+      },
+    }),
+    new InjectedConnector({
+      chains,
+      options: {
+        name: 'Injected',
+        shimDisconnect: true,
+      },
+    }),
+  ],
+  provider,
+  webSocketProvider,
+});
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <WagmiConfig client={client}>
+      <Component {...pageProps} />
+    </WagmiConfig>
+  );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,29 @@
+import type { NextPage } from 'next';
 import Head from 'next/head';
 
-export default function Home() {
+const Home: NextPage = () => {
   return (
     <>
       <Head>
         <title>Opencord Kyberswap Widgets Plugin</title>
-        <meta name="description" content="" />
+        <meta
+          name="description"
+          content="opencord plugin for kyberswap widgets"
+        />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main></main>
+      <main>
+        <h1 className="p-8 px-4 sm:px-8 py-2 sm:py-3 bg-sky-700 hover:bg-sky-800">
+          tailwind is shit
+        </h1>
+
+        <code className="rounded-md bg-gray-100 p-3 font-mono text-lg">
+          pages/index.tsx
+        </code>
+      </main>
     </>
   );
-}
+};
+
+export default Home;
